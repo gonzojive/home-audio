bdd4438cfb90cb7d8eb6d27a5da242ef0ab46838
Red Daly <reddaly@gmail.com>
2015-06-15 19:48:16 -0700

    Commit script to start shairplay instance in new tmux session and init.d script.
    - start-shairplay.sh starts a shairplay server
    
    - rc.local is a copy of the /etc/rc.local file on the Raspberry Pi.  This script
      executes on startup, ensuring the shairplay server starts whenever the machine
      boots up.

---
+++ start-shairplay.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Starting new tmux session: main"
+tmux new -d -s main
+tmux send-keys -t main 'cd /home/pi' ENTER
+tmux send-keys -t main '/usr/local/bin/shairplay --apname="AirPortPi" --password=""' ENTER
