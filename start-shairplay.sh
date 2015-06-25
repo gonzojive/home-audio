708235f844d717ba3acb7c37be54322071b97863
Red Daly <reddaly@gmail.com>
2015-06-25 05:53:27 +0000

    Add comment to start-shairplay to note how to prefer a particular audio output device.

--- start-shairplay.sh
+++ start-shairplay.sh
@@ -7,4 +7,5 @@ echo "apname: $APNAME"
 echo "hwaddr: $HWADDR"
 tmux new -d -s main
 tmux send-keys -t main "cd /home/pi/shairplay" ENTER
+# Note: Consider setting --ao_devicename="iec958:DAC", where "iec958:DAC" is obtained from `aplay -L`
 tmux send-keys -t main "/usr/local/bin/shairplay --apname=\"$APNAME\" --password=\"\" --hwaddr=\"$HWADDR\"" ENTER
