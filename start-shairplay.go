b64b1d244b4b03cbfda1a621b701f97d44a5bef3
Red Daly <reddaly@gmail.com>
2015-06-27 05:34:45 +0000

    Add Go wrapper script around shairplay to restart upon failure.

---
+++ start-shairplay.go
@@ -0,0 +1,66 @@
+// Program start-shairplay starts a Shairplay server and restarts it if it
+// crashes.
+package main
+
+import (
+	"fmt"
+	"hash/crc32"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net"
+	"os"
+	"os/exec"
+	"time"
+)
+
+const (
+	airplayNameFile    = "/home/pi/AIRPLAY_NAME"
+	sleepAfterFailTime = time.Second * 5
+)
+
+func genHardwareAddr(r *rand.Rand) net.HardwareAddr {
+	b := make([]byte, 6)
+	for i := 0; i < len(b); i++ {
+		b[i] = byte(r.Int31n(256))
+	}
+	return net.HardwareAddr(b)
+}
+
+func randFromName(s string) *rand.Rand {
+	return rand.New(rand.NewSource(int64(crc32.ChecksumIEEE([]byte(s)))))
+}
+
+func runCmdOnce() error {
+	nameBytes, err := ioutil.ReadFile(airplayNameFile)
+	if err != nil {
+		log.Fatalf("Error reading Airplay device name: %v", err)
+	}
+	name := string(nameBytes)
+
+	// Note: Consider setting --ao_devicename="iec958:DAC", where
+	// "iec958:DAC" is obtained from `aplay -L`
+	args := []string{
+		fmt.Sprintf("--apname=%s", name),
+		"--password=",
+		fmt.Sprintf("--hwaddr=%s", genHardwareAddr(randFromName(name))),
+	}
+	cmd := exec.Command("shairplay", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	log.Printf("Starting shairplay with args: %q", args)
+	return cmd.Run()
+}
+
+func main() {
+	for {
+		if err := runCmdOnce(); err != nil {
+			log.Printf("Error during run: %v", err)
+			log.Printf("Sleeping for %v before restarting...", sleepAfterFailTime)
+			time.Sleep(sleepAfterFailTime)
+		} else {
+			break
+		}
+	}
+}
--- start-shairplay.sh
+++ start-shairplay.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 echo "Starting new tmux session: main"
-PI_HOME=/home/pi
-APNAME=`cat $PI_HOME/AIRPLAY_NAME`
-HWADDR=`python $PI_HOME/home-audio/gen_hwaddr.py`
-echo "apname: $APNAME"
-echo "hwaddr: $HWADDR"
 tmux new -d -s main
+
 tmux send-keys -t main "cd /home/pi/shairplay" ENTER
-# Note: Consider setting --ao_devicename="iec958:DAC", where "iec958:DAC" is obtained from `aplay -L`
-tmux send-keys -t main "/usr/local/bin/shairplay --apname=\"$APNAME\" --password=\"\" --hwaddr=\"$HWADDR\"" ENTER
+tmux send-keys "go run ../home-audio/start-shairplay.go" ENTER
+#tmux send-keys -t main "/usr/local/bin/shairplay --apname=\"$APNAME\" --password=\"\" --hwaddr=\"$HWADDR\"" ENTER
